f9e91554a7dc6e2ed705193ec3dec1e9342878d4
dave-ck <daveck@protonmail.com>
2019-11-29 18:17:20 +0000

    linear space MatrixBand class, full functional, with address conversion

--- banded_matrix.py
+++ banded_matrix.py
@@ -6,7 +6,7 @@ class MatrixBand:
     def __init__(self, max_diff, min_diff, len_t, dtype, default):
         self.max_diff = max_diff
         self.min_diff = min_diff
-        self.array = np.empty([max_diff - min_diff, len_t], dtype=dtype)
+        self.array = np.empty([max_diff - min_diff + 1, len_t], dtype=dtype)
         self.array[:] = default
 
     def readdress(self, i, j):
@@ -15,6 +15,9 @@ class MatrixBand:
             raise ValueError("Coordinates which were either negative or outside the band were passed.")
         i = i - j - self.min_diff
         return i, j
+
+    def revert_address(self, i, j):  # inverse of readdress function
+        return j + i + self.min_diff, j
 
     def get(self, i, j):
         return self.array[self.readdress(i, j)]
@@ -26,40 +29,26 @@ class MatrixBand:
         print("Array is now")
         print(self.array)
 
+    def max_value_coords(self):
+        best_score = np.amax(self.array)
+        best_mat = np.where(self.array == best_score)
+        i, j = best_mat[0][0], best_mat[1][0]
+        coords_out = self.revert_address(i, j)
+        if self.array[i, j] == 0:
+            coords_out = (0, 0)
+        return coords_out
 
 
-"""
 # TEST CODE BELOW - COMPARISON WITH "NAIVE" QUADRATIC-SPACE IMPLEMENTATION
 k = 3
-len_t = 8  # j indexed
+len_t = 15  # j indexed
 len_s = 10  # i indexed
-diff = 2
+diff = 0
 
-m_good = MatrixBand(diff + k + 1, diff - k, len_t, dtype='str', default='-')
-m_good.set(0, 0, 'C')
-m_good.set(1, 0, 'D')    # good
-m_good.set(3, 1, 'A')    # good
-m_good.set(8, 4, 'A')    # good
-m_good.set(0, 1, 'U')    # good
+m_good = MatrixBand(diff + k, diff - k, len_t, dtype='int', default=0)
+m_good.set(0, 0, 3)
+m_good.set(1, 0, 2)  # good
+m_good.set(3, 1, 3)  # good
+m_good.set(0, 1, 5)  # good
+m_good.max_value_coords()
 m_good.print()
-
-m_bad = np.array([[i - j - diff for i in range(len_t)] for j in range(len_s)])
-m_bad = abs(m_bad)
-m_bad[m_bad > abs(k)] = -1
-m_bad[m_bad > 0] = 1
-
-m_bad[0, 0] = 15
-m_bad[1, 1] = 15
-m_bad[6, 3] = 15
-print(m_bad)
-
-start = time.time()
-m_good = MatrixBand(diff + k, diff - k -1 , len_t)
-print("Generated linear-space band in: {}".format(time.time()-start))
-
-start = time.time()
-m_bad = np.zeros([len_t, len_s], dtype='int8')
-print("Generated quadratic-space matrix in: {}".format(time.time()-start))
-
-
-"""
