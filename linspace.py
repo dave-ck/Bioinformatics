7bacafe2efb4af80a527ca38c856d35a1ddc3ef2
dave-ck <daveck@protonmail.com>
2019-12-05 16:35:25 +0000

    fully (*pending tests) working

--- linspace.py
+++ linspace.py
@@ -6,17 +6,13 @@ import numpy as np
 # IMPORTANT: refactor and verify function signatures
 
 def dynproglin(alphabet, scoring_matrix, seq_s, seq_t, show_alignment=False):
-    # run through, find end points
-
-    # initialize 2 columns (or rows) each for matrix_v and backtrack; these alternate at each step
-    # backtrack contains tuples pointing to sequence start
-    # once run-through is complete, read endpoints for the optimal local alignment from pointers
-    # run linear space global alignment on the cropped (spliced) sequences
-
-    # re-adjust the output of global alignment by adding the starting coordinates to each index
-    # so i.e. "ABB" and "BBA" would produce [2, [0,1], [0,1]]
-    # for global alignment of "BB" and "BB" - make it [2, [1,2], [0,1]]
-    pass
+    # todo: chop off to make local
+    score, start_ptr, end_ptr = get_local_score_and_endpoints(alphabet, scoring_matrix, seq_s, seq_t)
+    start_s, start_t = start_ptr
+    end_s , end_t = end_ptr
+    indices_s, indices_t = hirschberg(sigma, score_matrix, seq_s[start_s:end_s], seq_t[start_t:end_t])
+    indices_s = LIS
+    return score, indices_s, indices_t
 
 
 # one-line function, keep for cleaner code
@@ -39,7 +35,6 @@ def needleman_wunsch_char_v_seq(alphabet, scoring_matrix, seq_s, seq_t):
     for i in range(len_s):
         all_indel_score += score(alphabet, scoring_matrix, seq_s[i], "_")
     best_score = all_indel_score
-    print("All indel:", all_indel_score)
     best_alignment_s = []
     best_alignment_t = []
     for i in range(len_s):
@@ -58,29 +53,30 @@ def needleman_wunsch_char_v_seq(alphabet, scoring_matrix, seq_s, seq_t):
 
 
 def hirschberg(alphabet, scoring_matrix, seq_s, seq_t):
-    if len(seq_t) != 1:
-        seq_s, seq_t = seq_t, seq_s
-        swapped = True  # keep track of whether s and t were swapped around to allow wlog assumption len_t == 1
-    else:
-        swapped = False
-    alignment_s = []
-    alignment_t = []
+    # if len(seq_s) > len(seq_t):
+    #     seq_s, seq_t = seq_t, seq_s
+    #     swapped = True  # keep track of whether s and t were swapped around to allow wlog assumption len_t > len_s
+    # else:
+    #     swapped = False
     if len(seq_s) == 1 or len(seq_t) == 1:
         alignment_s, alignment_t = needleman_wunsch_char_v_seq(alphabet, scoring_matrix, seq_s, seq_t)
     else:
-        xlen = len(X)
-        xmid = math.floor(len(X) / 2)
-        ylen = len(Y)
-        scoreL = get_global_score(alphabet, scoring_matrix, X[:xmid], Y)
-        revY = Y[::-1]
-        scoreR = get_global_score(alphabet, scoring_matrix, X[xmid:], revY)
-        sum_scores = scoreL + scoreR
-        print(np.where(sum_scores == np.amax(sum_scores))[0][0])
-        # ymid = arg max ScoreL + rev(ScoreR)
-        ymid = np.where(sum_scores == np.amax(sum_scores))[0][0]
-        Z1, W1 = hirschberg(alphabet, scoring_matrix, X[:xmid], Y[:ymid])
-        Z2, W2 = hirschberg(alphabet, scoring_matrix, X[xmid:], Y[ymid:])
-        Z, W = Z1 + Z2, W1 + W2
+        len_t = len(seq_t)
+        t_mid = math.floor(len_t / 2)
+        score_left = get_global_score(alphabet, scoring_matrix, seq_s, seq_t[:t_mid])
+        rev_s = seq_s[::-1]
+        score_right = get_global_score(alphabet, scoring_matrix, rev_s, seq_t[t_mid:])
+        score_right = list(reversed(score_right))
+        sum_scores = score_left + score_right
+        s_mid = np.where(sum_scores == np.amax(sum_scores))[0][0]
+        # todo: fix relative addressing
+        alignment_s_1, alignment_t_1 = hirschberg(alphabet, scoring_matrix, seq_s[:s_mid], seq_t[:t_mid])
+        alignment_s_2, alignment_t_2 = hirschberg(alphabet, scoring_matrix, seq_s[s_mid:], seq_t[t_mid:])
+        alignment_s_2 = list(map(lambda x: x + s_mid, alignment_s_2))
+        alignment_t_2 = list(map(lambda x: x + t_mid, alignment_t_2))
+        alignment_s, alignment_t = alignment_s_1 + alignment_s_2, alignment_t_1 + alignment_t_2
+    # if swapped:
+    #     alignment_t, alignment_s = alignment_s, alignment_t
     return alignment_s, alignment_t
 
 
@@ -98,36 +94,27 @@ def get_local_score_and_endpoints(alphabet, scoring_matrix, seq_s, seq_t):
         backtrack[0, 1] = (0, j)
         for i in range(1, len_s + 1):
             # fill column j (held at 1) using column j-1 (held at 0)
-            print()
-            print("i, j: {}, {}".format(i, j))
-            print("seq_s, seq_t: {}, {}".format(seq_s, seq_t))
-            print("seq_s[i-1:i+1], seq_t[j-1:j+1]: {}, {}".format(seq_s[i - 1:i + 1], seq_t[j - 1:j + 1]))
             best_for_cell = 0
             reset = True
             from_diagonal = matrix_v[i - 1, 0] + score(alphabet, scoring_matrix, seq_s[i - 1], seq_t[j - 1])
             from_left = matrix_v[i, 0] + score(alphabet, scoring_matrix, "_", seq_t[j - 1])
             from_up = matrix_v[i - 1, 1] + score(alphabet, scoring_matrix, seq_s[i - 1], "_")
             if best_for_cell <= from_diagonal:
-                print("From diagonal.")
                 best_for_cell = from_diagonal
-                print(backtrack)
                 backtrack[i, 1] = np.copy(backtrack[i - 1, 0])
                 matrix_v[i, 1] = best_for_cell
                 reset = False
             if best_for_cell <= from_left:
-                print("From left.")
                 best_for_cell = from_left
                 backtrack[i, 1] = np.copy(backtrack[i, 0])
                 matrix_v[i, 1] = best_for_cell
                 reset = False
             if best_for_cell <= from_up:
-                print("From up.")
                 best_for_cell = from_up
                 backtrack[i, 1] = np.copy(backtrack[i - 1, 1])
                 matrix_v[i, 1] = best_for_cell
                 reset = False
             if reset:
-                print("Reset.")
                 matrix_v[i, 1] = 0
                 backtrack[i, 1] = (i, j)
             if best_for_cell > best_score:
@@ -136,7 +123,6 @@ def get_local_score_and_endpoints(alphabet, scoring_matrix, seq_s, seq_t):
                 best_ptr_end = (i, j)
                 # print("Backtrack:\n", backtrack)
                 best_ptr_start = tuple(np.copy(backtrack[i, 1]))  # need to apply tuple to avoid pass-by-reference
-            print()
         # switch out column 1 for column 0
         matrix_v[:, [0, 1]] = matrix_v[:, [1, 0]]
         backtrack[:, [0, 1]] = backtrack[:, [1, 0]]
@@ -187,8 +173,8 @@ score_matrix = [[1, -5, -5, -5, -1],
                 [-5, -5, -5, 6, -4],
                 [-1, -1, -4, -4, -9]]
 
-seq_s = "CC"
-seq_t = "ACC"
+seq_s = "ADD"
+seq_t = "BBBBADD"
 
 # from wikipedia example: https://en.wikipedia.org/wiki/Hirschberg%27s_algorithm
 # sigma = "ACGT"
@@ -198,14 +184,13 @@ seq_t = "ACC"
 #                 [-1, -1, -1, 2, -2],
 #                 [-2, -2, -2, -2, -2]]
 #
-# seq_s = "AGTACGCA"
-# seq_t = "TATGC"
+# seq_s = "AACCCC"
+# seq_t = "CAACC"
 
 # todo: check that endpoints are correctly identified: issue example from DUO not consistent with indices returned
 # High-score segments of perfect matches separated by short segments of cheap indels.
-a = get_local_score_and_endpoints(sigma, score_matrix, seq_s, seq_t)
-print("Yeet")
-# a = hirschberg(sigma, score_matrix, seq_s, seq_t)
+# a = get_local_score_and_endpoints(sigma, score_matrix, seq_s, seq_t)
+a = dynproglin(sigma, score_matrix, seq_s, seq_t)
 # a = needleman_wunsch_char_v_seq(sigma, score_matrix, "C", "C")
-
+print("Yeet")
 print(a)
